c9000c508ab81c091a383ba63a662c4a9336916b
Dmytro Zharii <you@example.com>
2023-10-31 23:37:37 -0700

    update Tue 10/31/2023

--- aww-scripts/open-in-typora.ps1
+++ aww-scripts/open-in-typora.ps1
@@ -7,8 +7,13 @@ $rootDir = Resolve-Path (Join-Path $ThisScriptFolderPath "..")
 $blogDir = Join-Path $rootDir "blog"
 $wikiDir = Join-Path $rootDir "docs"
 
-Start-Process -NoNewWindow -FilePath "typora" -ArgumentList $blogDir
-Start-Process -NoNewWindow -FilePath "typora" -ArgumentList $wikiDir
+Write-Host "blogDir = $($blogDir)"
+Write-Host "wikiDir = $($wikiDir)"
+
+Start-Process -FilePath "typora" -ArgumentList $blogDir
+sleep 1
+
+Start-Process  -FilePath "typora" -ArgumentList $wikiDir
 
 Write-Host "Hello World"
 
