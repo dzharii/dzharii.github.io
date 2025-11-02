3c69e58cdc41897931de865967bb96a650784c6c
John Doe <johndoe@example.com>
2025-11-02 00:58:47 -0700

    Deploy website - based on 9921e7e5732d589af04bd11dc77b68801625c641

---
+++ assets/js/92999a1c.ae6a356b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkzharii=self.webpackChunkzharii||[]).push([[8442],{19378:function(e){e.exports=JSON.parse('{"permalink":"/blog/page/3","page":3,"postsPerPage":10,"totalPages":8,"totalCount":77,"previousPage":"/blog/page/2","nextPage":"/blog/page/4","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
---
+++ assets/js/98b657d8.edd358e7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkzharii=self.webpackChunkzharii||[]).push([[8486],{83431:function(e){e.exports=JSON.parse('{"permalink":"/blog/page/8","page":8,"postsPerPage":10,"totalPages":8,"totalCount":77,"previousPage":"/blog/page/7","nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
